abfe12660391ad751a09275807038c58669a92ff
Vinícius Dutra <vinicius.dutra@gmail.com>
2020-10-16 12:45:26 -0300

    sqlite setup and first migration

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import './database/connection';
+
+const app = express()
+app.use(express.json())
+app.listen(3333)
+
+app.get('/users', (request, response)=> {
+	return response.json({ message : "hAPPy"})
+})
